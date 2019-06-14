21c2aeeb9eccedec67b8842c987f5e8eb6b7a1c6
Christian Kaisermann <christian@kaisermann.me>
2019-06-14 17:22:02 -0300

    Add mobile web capability

--- pug.config.js
+++ pug.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   self: true,
-  ...require("./app.json"),
-  emojis: require("./emojis.json"),
+  ...require('./app.json'),
+  emojis: require('./emojis.json'),
   asset(assetPath) {
     if (process.env.SITE_URL) {
       return `//${process.env.SITE_URL}/${assetPath}?c=${Math.random() *
         10000}`;
     }
 
-    return `${process.env.SITE_URL || "."}/${assetPath}`;
-  }
+    return `${'.'}/${assetPath}`;
+  },
 };
