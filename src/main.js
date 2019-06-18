41c162ecb87e37678f49c20f0a0dda14b856f9af
Matheus Marques <matheusocmarques@gmail.com>
2019-06-18 00:13:00 -0300

    Fix copy on iOS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 function copyToClipboard(str) {
   const el = document.createElement('textarea');
   el.value = str;
-  el.setAttribute('readonly', '');
+  el.contentEditable = true;
+  el.readOnly = true;
   el.style.position = 'absolute';
   el.style.left = '-9999px';
+  
   document.body.appendChild(el);
-  el.select();
+
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+    const range = document.createRange();
+    range.selectNodeContents(el);
+
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    el.setSelectionRange(0, 999999);
+  } else {
+    el.select();
+  }
+
   document.execCommand('copy');
   document.body.removeChild(el);
 }
