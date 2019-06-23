cb84ae5d32737142cb2bc4d659697573430e3a99
Christian Kaisermann <christian@kaisermann.me>
2019-06-23 00:11:16 -0300

    Replace everything with sapper

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,120 @@
-import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
-import cjs from 'rollup-plugin-commonjs';
-import workbox from 'rollup-plugin-workbox-build';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import autoPreprocess from 'svelte-preprocess';
+import json from 'rollup-plugin-json';
+
+import pkg from './package.json';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const preprocess = autoPreprocess({
+  postcss: true,
+});
+
+const onwarn = (warning, onwarn) =>
+  (warning.code === 'CIRCULAR_DEPENDENCY' &&
+    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning);
 
 export default {
-  input: 'src/main.js',
-  output: {
-    format: 'iife',
-    file: 'public/bundle.js',
-  },
-  plugins: [
-    resolve(),
-    cjs(),
-    process.env.NODE_ENV === 'production' &&
-      workbox({
-        mode: 'generateSW',
-        options: {
-          swDest: 'public/sw.js',
-          globDirectory: 'public',
-          globPatterns: ['*.{js,ico,gif,html,json,css}'],
-        },
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      json(),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
       }),
-    terser(),
-  ],
+      svelte({
+        dev,
+        hydratable: true,
+        emitCss: true,
+        preprocess,
+      }),
+      resolve({
+        browser: true,
+      }),
+      commonjs(),
+
+      legacy &&
+        babel({
+          extensions: ['.js', '.mjs', '.html', '.svelte'],
+          runtimeHelpers: true,
+          exclude: ['node_modules/@babel/**'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: '> 0.25%, not dead',
+              },
+            ],
+          ],
+          plugins: [
+            '@babel/plugin-syntax-dynamic-import',
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                useESModules: true,
+              },
+            ],
+          ],
+        }),
+
+      !dev &&
+        terser({
+          module: true,
+        }),
+    ],
+
+    onwarn,
+  },
+
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      json(),
+      replace({
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      svelte({
+        generate: 'ssr',
+        dev,
+        preprocess,
+      }),
+      resolve(),
+      commonjs(),
+    ],
+    external: Object.keys(pkg.dependencies).concat(
+      require('module').builtinModules ||
+        Object.keys(process.binding('natives')),
+    ),
+
+    onwarn,
+  },
+
+  serviceworker: {
+    input: config.serviceworker.input(),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      commonjs(),
+      !dev && terser(),
+    ],
+
+    onwarn,
+  },
 };
---
+++ src/components/EmojiList.svelte
@@ -0,0 +1,52 @@
+<script>
+  import CopyBtn from './CopyButton.svelte';
+
+  export let emojis;
+</script>
+
+<style>
+  ul {
+    display: block;
+    margin: 0 0 0.8rem;
+    padding: 2rem 0 0;
+    column-count: 5;
+  }
+
+  li {
+    display: inline-block;
+    width: 100%;
+    padding: 0.8rem;
+  }
+
+  @media (max-width: 1280px) {
+    ul {
+      column-count: 4;
+    }
+  }
+
+  @media (max-width: 1020px) {
+    ul {
+      column-count: 3;
+    }
+  }
+
+  @media (max-width: 768px) {
+    ul {
+      column-count: 2;
+    }
+  }
+
+  @media (max-width: 480px) {
+    ul {
+      column-count: 1;
+    }
+  }
+</style>
+
+<ul>
+  {#each emojis as emoji}
+    <li>
+      <CopyBtn>{emoji}</CopyBtn>
+    </li>
+  {/each}
+</ul>
