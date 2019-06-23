a71dd373d01dc7893754349ba7f2e3238d5c4161
Christian Kaisermann <christian@kaisermann.me>
2019-06-23 00:14:22 -0300

    Unique ID for each version

--- pug.config.js
+++ pug.config.js
@@ -1,5 +1,7 @@
-const { domain } = require('./package.json');
+const { version, domain } = require('./package.json');
 const BASE_URL = process.env.NODE_ENV === 'production' ? `//${domain}` : '';
+
+const CACHE_BUST_ID = `${version}${Math.random() * 10000}`;
 
 module.exports = {
   self: true,
@@ -7,6 +9,6 @@ module.exports = {
   emojis: require('./emojis.json'),
   base_url: BASE_URL,
   asset(assetPath) {
-    return `${BASE_URL}/${assetPath}?c=${Math.random() * 10000}`;
+    return `${BASE_URL}/${assetPath}?c=${CACHE_BUST_ID}`;
   },
 };
